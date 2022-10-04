f7125f18fab30fe0521310ac4cd9e8312ac6ea47
Beatriz Câmara <beatrizslcam@gmail.com>
2022-10-04 13:16:23 -0300

    Refatoração

--- desarfio.ex
+++ desarfio.ex
@@ -17,40 +17,62 @@ defmodule Desafio do
         ) :: [%{String.t() => non_neg_integer()}]
 
   def split(lista_de_compras, emails) do
-   quantidade_emails = tamanho_lista_email(emails)
-   valor_total_lista(lista_de_compras,0)
-  |>atribuicao_compra(quantidade_emails,emails)
+    {lista_emails_real, quantidade_emails} = lista_emails(emails)
+    valor_total_lista(lista_de_compras,0)
+    |>repartir_valor(quantidade_emails,lista_emails_real)
 
   end
 
-  defp tamanho_lista_email(emails) do
-    Enum.frequencies(emails)
-    |>map_size()
+  defp lista_emails(emails) do
+    lista_emails_real  = Enum.frequencies(emails)|> Map.keys()
+    {lista_emails_real,length(lista_emails_real)}
   end
 
-  defp valor_total_lista([], valor_para dividir), do: valor_para_dividir
+  defp valor_total_lista([], valor_compra), do: valor_compra
 
-  defp valor_total_lista(lista_de_compras,valor_para_dividir) do
+  defp valor_total_lista(lista_de_compras,valor_compra) do
     [item| itens_restantes] = lista_de_compras
-    valor_para_dividir = valor_para_dividir + (elem(item,1)*elem(item,2))
-    valor_total_lista(itens_restantes, valor_para_dividir)
+    valor_compra = valor_compra + (elem(item,1)*elem(item,2))
+    valor_total_lista(itens_restantes, valor_compra)
   end
 
-  defp atribuicao_compra(valor_total,quantidade_emails,emails) do
+  def repartir_valor(valor_total,quantidade_emails,emails) do
     divisao = div(valor_total,quantidade_emails)
     resto = rem(valor_total,quantidade_emails)
-    case  divisao do
-      0 ->
-        rem(valor_total,quantidade_emails)
+    acc = 0
+    resultado = %{}
+    case  [divisao,resto] do
+      [0, resto]->
+        if (acc != 0) do
+          inserir_no_map(emails,1,resto)
 
-      _ && rem(valor_total,quantidade_emails) == 0 ->
-
-      _->
-
+          else
+            inserir_no_map(emails,0,_resto)
+        end
+    #  [divisao,0]->
+     #     inserir_no_map(emails,divisao)
+      [_divisao,_resto]->
+        :ok
+      end
     end
 
-  end
+    def inserir_no_map(emails,valor,_resto, _quantidade_emails) do
+      acc = 0
+      Enum.map(emails,
+      fn x ->
+        if(acc < quantidade_emails ), do: Map.put_new(%{},x,valor)
+        else
 
+      end)
+    end
 end
-##ToDO:
-#Finalizar a parte de atribuição e testar
+#emails = ~w(
+#  paulo@email.com
+#  valente@email.com
+#  teste@email.com
+#  valente@email.com
+#  paulo@email.com
+#  valente@email.com
+#)
+
+#Desafio.split([1,2,3], emails)
